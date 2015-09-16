dddd00df78b8d797552b4befa8dc54d81da81940
frandorr <fran.dorr@gmail.com>
2015-09-16 12:32:17 -0300

    Experimento entropia parcial

--- src/calculador_entropia.py
+++ src/calculador_entropia.py
@@ -6,13 +6,18 @@ from collections import Counter
 
 # Fuente S que distingue según tipo
 S = []
-
+Ssrc = []
 # Agrega símbolos a medida que se sniffean.
 # La fuente S que distingue tipos
 def add_symbol_to_S(pkt):
     global S
     S.append(pkt[Ether].type)
 
+def add_symbol_to_Ssrc(pkt):
+    global Ssrc
+    if ARP in pkt and pkt[ARP].op in (1,2):
+        Ssrc.append(pkt[ARP].psrc)
+        print pkt[ARP].hwsrc,",", pkt[ARP].psrc,",", pkt[ARP].pdst,",", entropy(Ssrc)
 # Calcula la entropia de una fuente dada como una lista de simbolos
 def entropy(source):
     entropy = 0.0
@@ -30,12 +35,12 @@ def show_ether(pkt):
     print pkt[Ether].src, pkt[Ether].dst, pkt[Ether].type
 
 # Función que sniffea red local
-def sniff_local(callback_function, intervalo=5):
+def sniff_local(callback_function, intervalo=5, filtro=""):
     """ Escucha pasivamente la red local y procesa los datos en
     la función callback pasada como parámetro durante el intervalo
     de t tiempo en segundos"""
-    sniff(prn=callback_function, store=0, timeout=intervalo)
+    sniff(prn=callback_function, store=0, timeout=intervalo, filter=filtro)
 
 if __name__ == '__main__':
-    sniff_local(add_symbol_to_S)
+    sniff_local(add_symbol_to_Ssrc, intervalo=200, filtro="arp")
     print entropy(S)
